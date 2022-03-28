76ea8af012c6df98f13d58ac56ce33748d261c71
unknown <brunoachon@hotmail.com>
2022-03-28 17:38:33 -0300

    Fim do Projeto

--- monty-hall/src/App.vue
+++ monty-hall/src/App.vue
@@ -1,7 +1,34 @@
 <template>
   <div id="app">
     <h1>Problema de Monty Hall</h1>
-    <Door number="1" :hasGift="false"/>
+    <div class="form">
+      <!-- <Door number="1" :hasGift="false"/> -->
+      <div v-if="!started">
+        <label for="portsAmount">Quantas Portas? </label>
+        <input
+          type="text"
+          id="portsAmount"
+          size="3"
+          v-model.number="portsAmount"
+        />
+      </div>
+      <div v-if="!started">
+        <label for="selectedPort">Qual a porta premiada? </label>
+        <input
+          type="text"
+          id="selectedPort"
+          size="3"
+          v-model.number="selectedPort"
+        />
+      </div>
+      <button v-if="!started" @click="started = true">Iniciar</button>
+      <button v-if="started" @click="started = false">Reiniciar</button>
+    </div>
+    <div class="doors" v-if="started">
+      <div v-for="i in portsAmount" :key="i">
+        <Door :hasGift="i === selectedPort" :number="i" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,24 +36,31 @@
 import Door from './components/Door'
 
 export default {
-  name: 'App',
-  components:{ Door }
-}
+  name: "App",
+  components:{ Door },
+  data: function () {
+    return {
+      started: false,
+      portsAmount: 3,
+      selectedPort: null,
+    };
+  },
+};
 </script>
 
 <style>
-*{
+* {
   box-sizing: border-box;
   font-family: "Montserrat", sans-serif;
 }
 
-body{
-  background: linear-gradient(to right, rgb(21,153,87), rgb(21,87,153));
+body {
+  background: linear-gradient(to right, rgb(21, 153, 87), rgb(21, 87, 153));
   color: #fff;
 }
 
-#app{
-  align-items: center;
+#app {
+  align-items: stretch;
   display: flex;
   flex-direction: column;
 }
@@ -38,4 +72,25 @@ body{
   margin-bottom: 60px;
 }
 
+.form{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 40px;
+}
+
+.form, 
+.form input,
+.form button{
+  margin-bottom: 10px;
+  font-size: 1.5rem;
+}
+
+.doors {
+  align-self: stretch;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
 </style>
