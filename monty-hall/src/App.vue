9ffade4f0a0ea0679781df122d46c5201afc6e10
unknown <brunoachon@hotmail.com>
2022-03-28 10:16:32 -0300

    confecção completa do gift

---
+++ monty-hall/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <h1>Problema de Monty Hall</h1>
+    <Gift />
+  </div>
+</template>
+
+<script>
+import Gift from './components/Gift'
+
+export default {
+  name: 'App',
+  components:{ Gift }
+}
+</script>
+
+<style>
+*{
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif;
+}
+
+body{
+  background: linear-gradient(to right, rgb(21,153,87), rgb(21,87,153));
+  color: #fff;
+}
+
+#app{
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
+
+#app h1 {
+  background-color: #0004;
+  border: 1px solid #000;
+  padding: 20px;
+  margin-bottom: 60px;
+}
+
+</style>
---
+++ monty-hall/src/components/Gift.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="gift">
+      <div class="gift-top"></div>
+      <div class="gift-body"></div>
+      <div class="gift-tie-1"></div>
+      <div class="gift-tie-2"></div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"gift-winner"
+}
+</script>
+
+<style>
+
+.gift {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    z-index: 10;
+}
+
+.gift-top {
+    background-color: #7aa944;
+    border: 1px solid #000;
+    height: 25px;
+    width: 100px;
+}
+
+.gift-body {
+    background-color: #5c7e32;
+    border-left: 1px solid #000;
+    border-right: 1px solid #000;
+    border-bottom: 1px solid #000;
+    height: 60px;
+    width: 90px;
+}
+
+.gift-tie-1{
+    background-color: red;
+    position: absolute;
+    top:1px;
+    height: 82px;
+    width: 15px;
+}
+
+.gift-tie-2{
+    background-color: red;
+    position: absolute;
+    height: 15px;
+    top:40px;
+    width: 88px;
+}
+
+</style>
